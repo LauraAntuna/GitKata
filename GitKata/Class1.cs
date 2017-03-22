296dda38d0c57b3b4276fa657d26a7caef7bfcdd
Laura Antuña <antuna.laura@gmail.com>
2017-03-22 21:36:23 +0000

    Add project files.

---
+++ GitKata/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitKata
+{
+    public class Class1
+    {
+    }
+}

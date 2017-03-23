9462e4c28d5c99b4de4407c74cb4d97ab805abea
Simon Petty <simon.petty@abide-financial.com>
2017-03-23 11:53:01 +0000

    This is fun

---
+++ GitKata.Tests/SillyFolder/TestyMcTestFace.cs
@@ -0,0 +1,10 @@
+﻿namespace GitKata.Tests.SillyFolder
+{
+    public class TestyMcTestFace
+    {
+        public int Splurge()
+        {
+            return 123456789;
+        }
+    }
+}
---
+++ GitKata.Tests/ThisIsAnotherFile.cs
@@ -0,0 +1,15 @@
+﻿using GitKata.Tests.SillyFolder;
+using Moq;
+
+namespace GitKata.Tests
+{
+    public class Class1Tests
+    {
+        private Mock<TestyMcTestFace> _myMoq;
+
+        public Class1Tests()
+        {
+            _myMoq = new Mock<TestyMcTestFace>();
+        }
+    }
+}
